ece2e66c92fb65ca489595ab61c717a146f6911c
Derek Johnson <dej3tc@virginia.edu>
2024-05-03 14:06:17 -0700

    change competition to have date range

--- src/routes/competition.rs
+++ src/routes/competition.rs
@@ -13,7 +13,8 @@ pub const TABLE_NAME: &'static str = "competitions";
 pub const ID_KEY: &'static str = "Id";
 const NAME_KEY: &'static str = "Name";
 const LOCATION_KEY: &'static str = "Location";
-const DATE_KEY: &'static str = "Date";
+const START_DATE_KEY: &'static str = "StartDate";
+const END_DATE_KEY: &'static str = "EndDate";
 
 // Define your Competition struct
 #[derive(Clone, Debug, Serialize, Deserialize)]
@@ -27,7 +28,8 @@ struct Competition {
 struct CompetitionData {
     name: String,
     location: String,
-    date: NaiveDate,
+    start_date: NaiveDate,
+    end_date: NaiveDate,
 }
 
 impl From<CompetitionData> for Competition {
@@ -53,14 +55,17 @@ impl Item for Competition {
         let id = Uuid::parse_str(id).unwrap();
         let name: String = map.get(NAME_KEY)?.as_s().unwrap().to_string();
         let location: String = map.get(LOCATION_KEY)?.as_s().unwrap().to_string();
-        let date_string: String = map.get(DATE_KEY)?.as_s().unwrap().to_string();
-        let date = NaiveDate::parse_from_str(&date_string, "%Y-%m-%d").unwrap();
+        let start_date_string: String = map.get(START_DATE_KEY)?.as_s().unwrap().to_string();
+        let start_date = NaiveDate::parse_from_str(&start_date_string, "%Y-%m-%d").unwrap();
+        let end_date_string: String = map.get(END_DATE_KEY)?.as_s().unwrap().to_string();
+        let end_date = NaiveDate::parse_from_str(&end_date_string, "%Y-%m-%d").unwrap();
         Some(Self {
             id,
             competition_data: CompetitionData {
                 name,
                 location,
-                date,
+                start_date,
+                end_date,
             },
         })
     }
@@ -77,8 +82,12 @@ impl Item for Competition {
             AttributeValue::S(self.competition_data.location),
         );
         map.insert(
-            DATE_KEY.to_string(),
-            AttributeValue::S(self.competition_data.date.to_string()),
+            START_DATE_KEY.to_string(),
+            AttributeValue::S(self.competition_data.start_date.to_string()),
+        );
+        map.insert(
+            END_DATE_KEY.to_string(),
+            AttributeValue::S(self.competition_data.end_date.to_string()),
         );
         map
     }
